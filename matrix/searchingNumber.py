a379cbd98e47f8ab3f5a0b3446165f00a111b79b
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-21 16:33:31 +0530

    Searching Number in Matrix with position

--- matrix/searchingNumber.py
+++ matrix/searchingNumber.py
@@ -5,12 +5,6 @@ arr = [
     [64, 66, 100, 122, 128]
 ]
 
-rows = len(arr)
-print("length of Array :" ,rows)
-
-cols= len(arr[0])
-print("Columns Length of Array :" , cols)
-
 # create a function for Searching an Element
 def searching_number(arr,num):
     rows,cols= len(arr),len(arr[0])
@@ -19,10 +13,13 @@ def searching_number(arr,num):
     for i in range(rows):
         for j in range(cols):
             if arr[i][j]==num:
-                return True
-    return False
+                return (i,j)
+    return None
 
-if searching_number(arr,8):
-    print("Yes Available Number")
+number=500
+position = searching_number(arr,number)
+
+if position:
+    print(f"Yes , number {number} is available at Position {position}")
 else:
-    print("Not Available Number")
+    print(f"Number {number} is not available")
